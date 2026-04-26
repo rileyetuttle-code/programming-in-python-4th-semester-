15063b41994a0078c43a9df3a81abe0350a83992
rileyetuttle-code <rileyetuttle@gmail.com>
2026-04-26 14:20:48 -0500

    changed a couple things

---
+++ w3schools_practice/try_except_example.py
@@ -0,0 +1,13 @@
+
+
+def main() -> None:
+    try:
+        print(x)
+    except:
+        print("An error occurred")
+    finally: 
+        print("Execution complete")
+
+
+if __name__ == "__main__":
+    main()
